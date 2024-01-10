1aa8a78bdaecb7495fcc0f00942f3fe52d24c447
IzcoGLeon <Izco.g.leon@outlook.com>
2024-01-09 19:56:29 -0600

    first commit

---
+++ src/org/generation/Codigo5.java
@@ -0,0 +1,48 @@
+package org.generation;
+
+import java.util.Scanner;
+
+/**
+ * El código toma un valor por parte del usuario en la consola a través de 
+ * Scanner, este valor de tipo int es almacenado en la variable ni, asi como 
+ * también la variable c, este ultimo con fines de impresión final del valor inicial
+ * para la consola. Mediante un loop while se declara una condición, la cual mientras
+ * sea verddera ejectuará el bloque de código dentro de este. Dentro de este bloque 
+ * encontramos que el valor introducido por el usuario mediante Scanner es sujeto a 
+ * una serie de operaciones y condiciones para determinar si el número es afortunado o no con 
+ * base a los resultados filtrados de cada último digito, hasta el punto que se llega al valor 0
+ * para salir del bucle.
+ */
+
+public class Codigo5 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int afo = 0;
+		int noAfo = 0;
+		System.out.print("Introduzca un número: ");
+		int ni = sc.nextInt();
+		int c = ni;
+
+		while (ni > 0) {
+			int digito = ni % 10; 
+			if ((digito == 3) || (digito == 7) || (digito == 9)) {
+				afo++;
+			} else {
+				noAfo++;
+			}
+			ni /= 10;
+			System.out.println(ni);
+		}
+		if (afo > noAfo) {
+			System.out.println("El " + c + " es un número afortunado.");
+		} else {
+			System.out.println("El " + c + " no es un número afortunado.");
+		}
+		
+		sc.close();
+	}
+
+}
